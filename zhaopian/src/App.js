b90e0169d9cfdbe2bad49163b6a81d401c5a847e
Qinqi Li <sumomoshinqi@Qinqis-MacBook-Pro.local>
2019-12-07 18:40:02 -0800

    add 2018/1

--- zhaopian/src/App.js
+++ zhaopian/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Card, Col, Row, Layout} from 'antd';
+import {Badge, Card, Col, Row, Layout} from 'antd';
 import './App.css';
 
 const {Meta} = Card;
@@ -11,27 +11,31 @@ class App extends Component {
       <div className="App">
         <Layout>
           <Content>
-            <div style={{display: 'flex',
+            <div style={{
+              display: 'flex',
               alignItems: 'center',
-              justifyContent: 'center', padding: '50px'}}>
-              <Row gutter={16}>
+              justifyContent: 'center', padding: '50px'
+            }}>
+              <Row gutter={50}>
                 <Col span={8}>
-                  <a target="_blank" href="https://www.icloud.com/sharedalbum/#B0wG6XBubGdvsij">
-                    <Card
-                      hoverable
-                      style={{width: 300}}
-                      cover={<img src="coverImg/USdaily.jpg"/>}
-                    >
-                      <Meta title="美国日常" description="持续更新"/>
-                    </Card>
-                  </a>
+                  <Badge dot={true}>
+                    <a target="_blank" href="https://www.icloud.com/sharedalbum/#B0wG6XBubGdvsij">
+                      <Card
+                        hoverable
+                        style={{width: 300}}
+                        cover={<img src="coverImg/USdaily.jpeg"/>}
+                      >
+                        <Meta title="美国日常" description="持续更新"/>
+                      </Card>
+                    </a>
+                  </Badge>
                 </Col>
                 <Col span={8}>
                   <a target="_blank" href="https://www.icloud.com/sharedalbum/#B0w5nhQSTGQAqpV">
                     <Card
                       hoverable
                       style={{width: 300}}
-                      cover={<img src="coverImg/Fr2018.JPG"/>}
+                      cover={<img src="coverImg/Fr2018.jpeg"/>}
                     >
                       <Meta title="2019年8月 法国" description="🇫🇷 🏖️ 巴黎 尼斯 摩纳哥"/>
                     </Card>
@@ -42,9 +46,28 @@ class App extends Component {
                     <Card
                       hoverable
                       style={{width: 300}}
-                      cover={<img src="coverImg/Wed.JPG"/>}
+                      cover={<img src="coverImg/Wed.jpeg"/>}
                     >
                       <Meta title="婚纱照" description="2018.02.15(农历新年) 领证"/>
+                    </Card>
+                  </a>
+                </Col>
+              </Row>
+            </div>
+            <div style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center', padding: '50px'
+            }}>
+              <Row gutter={50}>
+                <Col span={8}>
+                  <a target="_blank" href="https://www.icloud.com/sharedalbum/#B0w5n8hH4GQjS0J">
+                    <Card
+                      hoverable
+                      style={{width: 300}}
+                      cover={<img src="coverImg/us18.jpeg"/>}
+                    >
+                      <Meta title="2018年1月 美国" description="旧金山 洛杉矶 拉斯维加斯"/>
                     </Card>
                   </a>
                 </Col>
