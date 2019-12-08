a5fcc79fcf205cf502e9c4bb738daf24f9ccd063
Qinqi Li <sumomoshinqi@Qinqis-MacBook-Pro.local>
2019-12-07 18:43:48 -0800

    resize

--- zhaopian/src/App.js
+++ zhaopian/src/App.js
@@ -16,13 +16,13 @@ class App extends Component {
               alignItems: 'center',
               justifyContent: 'center', padding: '50px'
             }}>
-              <Row gutter={50}>
+              <Row gutter={80}>
                 <Col span={8}>
                   <Badge dot={true}>
                     <a target="_blank" href="https://www.icloud.com/sharedalbum/#B0wG6XBubGdvsij">
                       <Card
                         hoverable
-                        style={{width: 300}}
+                        style={{width: 240}}
                         cover={<img src="coverImg/USdaily.jpeg"/>}
                       >
                         <Meta title="美国日常" description="持续更新"/>
@@ -34,7 +34,7 @@ class App extends Component {
                   <a target="_blank" href="https://www.icloud.com/sharedalbum/#B0w5nhQSTGQAqpV">
                     <Card
                       hoverable
-                      style={{width: 300}}
+                      style={{width: 240}}
                       cover={<img src="coverImg/Fr2018.jpeg"/>}
                     >
                       <Meta title="2019年8月 法国" description="🇫🇷 🏖️ 巴黎 尼斯 摩纳哥"/>
@@ -45,7 +45,7 @@ class App extends Component {
                   <a target="_blank" href="https://www.icloud.com/sharedalbum/#B0wGY8gBYjQjGq">
                     <Card
                       hoverable
-                      style={{width: 300}}
+                      style={{width: 240}}
                       cover={<img src="coverImg/Wed.jpeg"/>}
                     >
                       <Meta title="婚纱照" description="2018.02.15(农历新年) 领证"/>
@@ -64,7 +64,7 @@ class App extends Component {
                   <a target="_blank" href="https://www.icloud.com/sharedalbum/#B0w5n8hH4GQjS0J">
                     <Card
                       hoverable
-                      style={{width: 300}}
+                      style={{width: 240}}
                       cover={<img src="coverImg/us18.jpeg"/>}
                     >
                       <Meta title="2018年1月 美国" description="旧金山 洛杉矶 拉斯维加斯"/>
